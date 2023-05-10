e9ed3a80fb99eaabce8f4f55ce79b4dbbcff9590
Joel Isaias Condori Leon <jcondorile@unsa.edu.pe>
2023-05-10 15:07:44 -0400

    Mejora del Holamundo.java

--- holaMundo.java
+++ holaMundo.java
@@ -4,7 +4,10 @@ public class HolaMundo1 {
     public static void main(String[] args) {
 	
 	System.out.println("Hola mundo");
-        
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Escriba su nombre --> ");
+        String n=scan.nextLine();
+        System.out.println("Bienvenido "+ n)
     }
     
 }
